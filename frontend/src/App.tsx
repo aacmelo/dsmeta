2304e08253f50727962c9ca80e8be5220195c297
aacmelo <andreaparecidocorreia@gmail.com>
2022-09-13 20:24:40 -0300

    Other Components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,19 @@
-import NotificationButton from "./components/NotificationButton"
+import Header from "./components/Header"
+import SalesCard from "./components/SalesCard"
 
 function App() {
   return(
     <>
-    <h1>Olá Mundo</h1>
-    <NotificationButton/>
-    <NotificationButton/>
+    <Header/>
+    <SalesCard/>
+    <main>
+    <section id="sales">
+        <div className="dsmeta-container">
+          
+          </div>
+          </section>
+          </main>
+
     </>
   )
 }
